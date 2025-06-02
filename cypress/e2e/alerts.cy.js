00457af989059180a620ea9f602dc6a7c72ee8e7
sofi <osinskaasona@gmail.com>
2025-06-02 22:42:53 +0300

    fix

--- cypress/e2e/alerts.cy.js
+++ cypress/e2e/alerts.cy.js
@@ -2,49 +2,52 @@
 
 describe('Alerts testing', () => {
 
-    beforeEach(() => {
-      cy.visit('https://demoqa.com/alerts');
-  
-      Cypress.on('uncaught:exception', () => false);
+  beforeEach(() => {
+    cy.visit('https://demoqa.com/alerts');
+    Cypress.on('uncaught:exception', (err, runnable) => {
+      console.log("Uncaught error:", err.message);
+      return false; 
     });
-  
-    it('handles alert with OK button', () => {
-      cy.get('#alertButton').click();
-      cy.on('window:alert', (text) => {
-        expect(text).to.contains('You clicked a button');
-      });
+  });
+
+  it('handles alert with OK button', () => {
+    cy.wait(500);
+    cy.get('#alertButton').click();
+    cy.on('window:alert', (text) => {
+      expect(text).to.contains('You clicked a button');
     });
-  
-    it('handles delayed alert', () => {
-      cy.get('#timerAlertButton').click();
-      cy.on('window:alert', (text) => {
-        expect(text).to.contains('This alert appeared after 5 seconds');
-      });
+  });
+
+  it('handles delayed alert', () => {
+    cy.wait(500);
+    cy.get('#timerAlertButton').click();
+    cy.on('window:alert', (text) => {
+      expect(text).to.contains('This alert appeared after 5 seconds');
     });
-  
-    it('handles confirm alert - OK', () => {
-      cy.window().then((win) => {
-        cy.stub(win, 'confirm').returns(true);
-      });
-      cy.get('#confirmButton').click();
-      cy.get('#confirmResult').should('contain', 'You selected Ok');
+  });
+
+  it('handles confirm alert - OK', () => {
+    cy.window().then((win) => {
+      cy.stub(win, 'confirm').returns(true);
     });
-  
-    it('handles confirm alert - Cancel', () => {
-      cy.window().then((win) => {
-        cy.stub(win, 'confirm').returns(false);
-      });
-      cy.get('#confirmButton').click();
-      cy.get('#confirmResult').should('contain', 'You selected Cancel');
+    cy.get('#confirmButton').click();
+    cy.get('#confirmResult').should('contain', 'You selected Ok');
+  });
+
+  it('handles confirm alert - Cancel', () => {
+    cy.window().then((win) => {
+      cy.stub(win, 'confirm').returns(false);
     });
-  
-    it('handles prompt alert', () => {
-      cy.window().then((win) => {
-        cy.stub(win, 'prompt').returns('Cypress Test');
-      });
-      cy.get('#promtButton').click();
-      cy.get('#promptResult').should('contain', 'Cypress Test');
+    cy.get('#confirmButton').click();
+    cy.get('#confirmResult').should('contain', 'You selected Cancel');
+  });
+
+  it('handles prompt alert', () => {
+    cy.window().then((win) => {
+      cy.stub(win, 'prompt').returns('Cypress Test');
     });
-    
+    cy.get('#promptButton').click();
+    cy.get('#promptResult').should('contain', 'Cypress Test');
   });
   
+});
